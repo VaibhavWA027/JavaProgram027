2e2bcc966528c64a11b92881ce3ab24624ff90d1
VaibhavWA027 <vaibhavkumar.cs23@bmsce.ac.in>
2024-10-01 12:48:42 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+class forloop{
+	public static void main(String args[]){
+			int x;
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter your value:");
+			x=sc.nextInt();
+			int i=1;
+			for(i=1;i<=x;i++){
+				System.out.println(i);
+				
+			}
+			
+
+	}
+}
